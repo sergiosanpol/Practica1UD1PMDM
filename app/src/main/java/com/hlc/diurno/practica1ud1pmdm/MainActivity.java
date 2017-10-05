2cda4b2c910095ff27c73438eee84f461b1b4386
sergiosanpol <sergiosanpol@gmail.com>
2017-10-05 09:27:43 +0200

    Personalizados los iconos de la App

--- app/src/main/java/com/hlc/diurno/practica1ud1pmdm/MainActivity.java
+++ app/src/main/java/com/hlc/diurno/practica1ud1pmdm/MainActivity.java
@@ -3,11 +3,16 @@ package com.hlc.diurno.practica1ud1pmdm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * @Author Sergio Sánchez Polaino
+ *
+ * Clase principal de la APP. Carga la vista activity_main.xml que contiene mi nombre, apellidos y curso actual
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Asigna la vista que debe mostrar
     }
 }
